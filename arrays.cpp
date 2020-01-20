874958f0fae50092837ab836fddd358f8df9c1d8
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-20 14:29:13 +0530

    added an aray problem

---
+++ arrays.cpp
@@ -0,0 +1,19 @@
+//
+// Created by xesandeep on 2020-01-20.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] ;
+    for (int i =0 ; i<5 ; i++) {
+        cin>>arr[i];
+    }
+    cout<<endl;
+    for (int i =0 ; i<5 ; i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+}
