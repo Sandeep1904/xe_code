32f88b231dee1c4eae5b3f49ad0e2e49e3e1d149
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-28 17:41:38 +0530

    introduced a bubble sort

--- sorting.cpp
+++ sorting.cpp
@@ -7,6 +7,7 @@ using namespace std;
 // this is for selection sort algorithm.
 void selection(int arr[], int size){
     int min, temp;
+    cout<<"this is done by selection sort"<<endl;
     for (int i=0;i<size-1;i++){
         min = i;
         for (int j =i+1;j<size;j++){
@@ -30,6 +31,7 @@ void selection(int arr[], int size){
 
 void bubble(int arr[], int size){
     int temp;
+    cout<<"this is done by bubble sort"<<endl;
     for (int i = 0 ;i<size-1;i++){
         for (int j = 0 ;j<size-i-1;j++) {
             if (arr[j] > arr[j + 1]) {
