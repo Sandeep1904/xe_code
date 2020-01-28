a6c3a66c65583dcf1fdf802277df45d6406af772
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-28 17:15:30 +0530

    jghf

---
+++ sorting.cpp
@@ -0,0 +1,31 @@
+//
+// Created by xesandeep on 2020-01-28.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {5,4,3,2,1};
+    int size = 5;
+    int min, temp;
+    for (int i =0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    for (int i=0;i<size-1;i++){
+        for (int j =i+1;j<size;j++){
+            if (arr[min] = arr[i]){
+                min = j;
+
+            }
+            temp = arr[min];
+            arr[min]= arr[i];
+            arr[i] = temp;
+        }
+    }
+    for (int i =0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+
+}
