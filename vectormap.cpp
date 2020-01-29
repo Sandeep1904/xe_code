5706c9d539188a185017d59edc053e52ea4e1985
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-29 16:12:49 +0530

    introduced a vectors and maps

--- vectormap.cpp
+++ vectormap.cpp
@@ -1,37 +1,4 @@
-//////
-////// Created by xesandeep on 2020-01-29.
-//////
-////
-//#include <iostream>
-//#include <vector>
-//
-//using namespace std;
-//
-//int main() {
-//    vector<int> vec;
-//
-//    for (int i=1;i<=5;i++)
-//        vec.push_back(i);
-//
-//    cout<<"output of the begin and the end: ";
-//    for (auto i = vec.begin();i!= vec.end(); i++)
-//        cout<<*i<<" ";
-//
-//    cout<<"\noutput of the cbegin and cend: ";
-//    for (auto i =vec.cbegin();i!=vec.cend();i++)
-//        cout<<*i<<" ";
-//
-//    cout<<"\noutput of the rbegin and rend: ";
-//    for (auto ir = vec.rbegin();ir!= vec.rend;++ir)
-//        cout<<*ir<<" ";
-//
-//    cout<<"\noutput of the crbegin  and crend: ";
-//    for (auto ir = vec.crbegin ; ir!= vec.crend ; ++ir)
-//        cout<<*ir<<" ";
-//
-//    return 0;
-//}
-//
+
 //// C++ program to illustrate the
 //// iterators in vector
 #include <iostream>
