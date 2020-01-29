58d2d1853087f833549d32d8f2cf7c4069a0a527
Sandeep1904 <sandeephugar19@gmail.com>
2020-01-29 16:49:36 +0530

    introduced a vectors and maps

--- vectormap.cpp
+++ vectormap.cpp
@@ -57,6 +57,44 @@ int main()
     int* pos = g1.data();
     cout<<"the first element is "<<*pos<<endl;
 
+    vector<int> v;
+    v.assign(5,10);
+    cout<<"the vector elements are: ";
+    for (int i= 0;i<v.size();i++)
+        cout<<v[i]<<" ";
+    cout<<endl;
 
-    return 0;
+    v.pop_back();
+    cout << "\nThe vector elements are: ";
+    for (int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    v.insert(v.begin(),5);
+    cout<<"the first element is: "<<v[0]<<endl;
+
+    v.erase(v.begin());
+    cout<<"the first element is: "<<v[0]<<endl;
+
+    v.emplace_back(20);
+    int n = v.size();
+    cout<<"the last element is: "<<v[n-1]<<endl;
+
+    v.clear();
+    cout<<"the vector size after erasing is: "<<n<<endl;
+
+    vector<int> g2;
+    for (int i=1;i<=5;i++){
+        g2.push_back(i*2);
+    }
+    g1.swap(g2);
+    cout << "\nAfter Swap \nVector 1: ";
+    for (int i = 0; i < g1.size(); i++)
+        cout << g1[i] << " ";
+
+    cout << "\nVector 2: ";
+    for (int i = 0; i < g2.size(); i++)
+        cout << g2[i] << " ";
+    cout<<endl;
+
+
+return 0;
 }
